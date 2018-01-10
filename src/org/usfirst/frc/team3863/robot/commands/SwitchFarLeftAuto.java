1c8eb84b2404277605735672750edf549bd0a502
rogerfachini <r3fachini@live.com>
2018-01-10 15:30:10 -0800

    Create near and far Switch autonomous modes

---
+++ src/org/usfirst/frc/team3863/robot/commands/SwitchFarLeftAuto.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team3863.robot.commands;
+
+import org.usfirst.frc.team3863.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *Autonomous for placing a power cube in the Switch, when our switch side is the opposite of our starting side. 
+ * This auton should be written for the Left side of the field.
+ * if start_right is True, then left-right values should be inverted (using right_invert), as we are starting on the right side of the field
+ */
+public class SwitchFarLeftAuto extends Command {
+	int right_invert = 1;
+    public SwitchFarLeftAuto(boolean start_right) {
+        requires(Robot.kDrivetrain);
+        if (start_right) {
+        	right_invert = -1;
+        }
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	if (right_invert == 1) {
+    		System.out.println("Auton: Score in FARTHEST switch side, starting on LEFT of field");
+    	} else if (right_invert == -1) {
+    		System.out.println("Auton: Score in FARTHEST switch side, starting on RIGHT of field");
+    	}
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
