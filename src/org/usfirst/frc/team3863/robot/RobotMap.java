860da936b170a5e353f7728b1a5ed14de53ebe54
rogerfachini <r3fachini@live.com>
2018-01-13 14:32:29 -0800

    Basic drivetrain implementation with PercentOutput

--- src/org/usfirst/frc/team3863/robot/RobotMap.java
+++ src/org/usfirst/frc/team3863/robot/RobotMap.java
@@ -14,13 +14,9 @@ package org.usfirst.frc.team3863.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	//Drivetrain CAN IDs
+	public static int TALON_DRIVE_LEFTA_ID = 4;
+	public static int TALON_DRIVE_LEFTB_ID = 5;
+	public static int TALON_DRIVE_RIGHTA_ID = 1;
+	public static int TALON_DRIVE_RIGHTB_ID = 3;
 }
--- src/org/usfirst/frc/team3863/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3863/robot/subsystems/Drivetrain.java
@@ -2,23 +2,35 @@ package org.usfirst.frc.team3863.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import org.usfirst.frc.team3863.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 /**
  Controls the four CANTalons dedicated to the Drivetrain
  */
 public class Drivetrain extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	WPI_TalonSRX talonLeftA = new WPI_TalonSRX(RobotMap.TALON_DRIVE_LEFTA_ID);
+	WPI_TalonSRX talonLeftB = new WPI_TalonSRX(RobotMap.TALON_DRIVE_LEFTB_ID);
+	WPI_TalonSRX talonRightA = new WPI_TalonSRX(RobotMap.TALON_DRIVE_RIGHTA_ID);
+	WPI_TalonSRX talonRightB = new WPI_TalonSRX(RobotMap.TALON_DRIVE_RIGHTB_ID);
+	
+	ControlMode mode = ControlMode.PercentOutput;
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
     
-    public void setDrivePower(float left, float right) {
+    public void setDrivePower(double left, double right) {
     	//Set the left and right motor power 
-    	//TODO: Actually implement this with CANTalons
-    	System.out.println("LeftMotorPower: " + left + " RightMotorPower: %s" + right);
+    	talonLeftA.set(mode, left);
+    	talonLeftB.set(mode,left);
+    	
+    	talonRightA.set(mode, right);
+    	talonRightB.set(mode, right);
     }
+    
 }
 
