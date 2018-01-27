da201fdde56076868d087571f6d0611f5fe50288
rogerfachini <r3fachini@live.com>
2018-01-26 16:35:40 -0800

    Elevator PID now supports zeroing and soft limits

--- src/org/usfirst/frc/team3863/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team3863/robot/subsystems/Elevator.java
@@ -16,7 +16,6 @@ public class Elevator extends Subsystem {
 	public int target = 0;
 
 	public WPI_TalonSRX elevDriveTalon = new WPI_TalonSRX(RobotMap.ELEVATOR_DRIVE_TALON_ID);
-	ControlMode mode = ControlMode.Position;
 	int pid_id = 0;
 	public int timeout_ms = 0;
 
@@ -31,31 +30,55 @@ public class Elevator extends Subsystem {
     
     //Initalize PID settings
     public void initPID() {
+    	setTargetPosition(0);
     	elevDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, pid_id, timeout_ms);
     	elevDriveTalon.setSensorPhase(false);
     	elevDriveTalon.configNominalOutputForward(0, timeout_ms);
     	elevDriveTalon.configNominalOutputReverse(0, timeout_ms);
     	elevDriveTalon.configPeakOutputForward(1, timeout_ms);
     	elevDriveTalon.configPeakOutputReverse(-1, timeout_ms);
+    	elevDriveTalon.configContinuousCurrentLimit(8, timeout_ms);
 
     	elevDriveTalon.configAllowableClosedloopError(0, pid_id, timeout_ms); 
 
     	elevDriveTalon.config_kF(pid_id, 0.0, timeout_ms);
-    	elevDriveTalon.config_kP(pid_id, 1.1, timeout_ms);
+    	elevDriveTalon.config_kP(pid_id, 1.5, timeout_ms);
         elevDriveTalon.config_kI(pid_id, 0.0, timeout_ms);
         elevDriveTalon.config_kD(pid_id, 0.0, timeout_ms);
     }
     
     public void setTargetPosition(int new_target) {
+    	//hard limit: 5600 ticks
+    	if (new_target > 5600) {
+    		new_target = 5600;
+    	}
         target = new_target;
-    	elevDriveTalon.set(mode, new_target);
+    	elevDriveTalon.set(ControlMode.Position, new_target);
     	System.out.println("Elevator Target Pos: " + new_target + " | pos: " + elevDriveTalon.getSelectedSensorPosition(timeout_ms));
+    }
+    
+    public void setMotorPower(double power) {
+    	elevDriveTalon.set(ControlMode.PercentOutput, power);
     }
     
     //Used by driving control systems to set speed limits. 
     public boolean isLiftRaised() {
-    	return false;
+    	return (elevDriveTalon.getSelectedSensorPosition(timeout_ms) > 100);
     }
+    
+    public boolean isLiftLowered() {
+    	return elevDriveTalon.getSensorCollection().isRevLimitSwitchClosed();
+    }
+    
+    public boolean testMotorCurrentThreshold(double amps) {
+    	return elevDriveTalon.getOutputCurrent() > amps;
+    }
+
+	public void zeroEncoder() {
+		target = 0;
+		elevDriveTalon.setSelectedSensorPosition(0, pid_id, timeout_ms);
+		System.out.println("Elevator encoder zeroed!");
+	}
     
     
 }
