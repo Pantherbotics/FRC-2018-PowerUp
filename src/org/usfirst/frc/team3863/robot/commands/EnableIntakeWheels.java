2b392bb6db3a6127cc88acd8a87663ada23b1e8f
rogerfachini <r3fachini@live.com>
2018-02-19 16:53:57 -0800

    Fix intake state

--- src/org/usfirst/frc/team3863/robot/commands/EnableIntakeWheels.java
+++ src/org/usfirst/frc/team3863/robot/commands/EnableIntakeWheels.java
@@ -10,39 +10,39 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class EnableIntakeWheels extends Command {
 	boolean reverse;
+	boolean canfin = false;
     public EnableIntakeWheels(boolean reverse_direction) {
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.kIntake);
         reverse = reverse_direction;
+        requires(Robot.kIntake);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
     	if (reverse) {
     		Robot.kIntake.setIntakeWheelPower(-Constants.INTAKE_MOTOR_POWER);
     	}else {
     		Robot.kIntake.setIntakeWheelPower(Constants.INTAKE_MOTOR_POWER);
     	}
-    	
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return canfin;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	
+    	Robot.kIntake.setIntakeWheelPower(0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
     	Robot.kIntake.setIntakeWheelPower(0);
+    	canfin = true;
     }
 }
