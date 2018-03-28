19d310c82f824a946c30b71bb9cf22d4a6e6ef46
Aaron Fang <aaron45765@gmail.com>
2018-03-27 20:58:45 -0700

    add finish condition to climb command

--- src/org/usfirst/frc/team3863/robot/commands/RunClimber.java
+++ src/org/usfirst/frc/team3863/robot/commands/RunClimber.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class RunClimber extends Command {
 
+	boolean fin;
     public RunClimber() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -17,6 +18,7 @@ public class RunClimber extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	fin=false;
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -26,16 +28,19 @@ public class RunClimber extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return fin;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.kClimber.setWinchPower(0);
+    	fin=true;
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
     	Robot.kClimber.setWinchPower(0);
+    	fin=true;
     }
 }
