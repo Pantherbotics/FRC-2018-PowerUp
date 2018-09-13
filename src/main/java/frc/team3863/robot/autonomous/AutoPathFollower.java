73b9b7f5def46aa0d54e38cd7ce8e593f2b686d5
Aaron Fang <aaron45765@gmail.com>
2018-09-12 18:03:25 -0700

    Revert Notifier in Path Follower

--- src/main/java/frc/team3863/robot/autonomous/AutoPathFollower.java
+++ src/main/java/frc/team3863/robot/autonomous/AutoPathFollower.java
@@ -12,8 +12,6 @@ public class AutoPathFollower extends Command  {
 
     private double start;
     private RamseteFollower follower;
-    private Notifier followerThread;
-    private volatile boolean isFinished;
 
     public AutoPathFollower(Trajectory traj) throws NullPointerException{
         requires(Robot.kDrivetrain);
@@ -26,28 +24,23 @@ public class AutoPathFollower extends Command  {
         Robot.kDrivetrain.setTransmissionHigh();
         Robot.kDrivetrain.setOdometry(follower.getInitOdometry());
         start = System.nanoTime();
-        followerThread = new Notifier(()->{
-            follower.setOdometry(Robot.kDrivetrain.getOdometry());
-            System.out.println("Getting next DriveSignal");
-            System.out.println(follower.getNextDriveSignal().getLeft() + ", " + follower.getNextDriveSignal().getRight());
-            Robot.kDrivetrain.setFPS(follower.getNextDriveSignal().getLeft(), follower.getNextDriveSignal().getRight());
-            isFinished = follower.isFinished();
-        });
-        followerThread.startPeriodic(.02);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        follower.setOdometry(Robot.kDrivetrain.getOdometry());
+        System.out.println("Getting next DriveSignal");
+        System.out.println(follower.getNextDriveSignal().getLeft() + ", " + follower.getNextDriveSignal().getRight());
+        Robot.kDrivetrain.setFPS(follower.getNextDriveSignal().getLeft(), follower.getNextDriveSignal().getRight());
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return isFinished;
+        return follower.isFinished();
     }
 
     // Called once after isFinished returns true
     protected void end() {
-        followerThread.stop();
         System.out.println("Finished!");
         System.out.println("Time to completion: " + ((System.nanoTime()-start)/1000000));
     }
