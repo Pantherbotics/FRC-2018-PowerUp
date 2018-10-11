7e7feb0a1897ba1d0a812c7595b04906ebc2807c
aaron <aaron45765@gmail.com>
2018-10-11 14:31:00 -0700

    Put Ramsete in its own thread (again)

--- src/main/java/frc/team3863/robot/autonomous/AutoPathFollower.java
+++ src/main/java/frc/team3863/robot/autonomous/AutoPathFollower.java
@@ -1,5 +1,6 @@
 package frc.team3863.robot.autonomous;
 
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team3863.robot.Constants;
 import frc.team3863.robot.Robot;
@@ -10,34 +11,42 @@ public class AutoPathFollower extends Command  {
 
     private double start;
     private RamseteFollower follower;
+    private Notifier trackingThread;
+    private volatile boolean isFinished;
 
     public AutoPathFollower(Trajectory traj) throws NullPointerException{
         requires(Robot.kDrivetrain);
         System.out.println(traj.length());
         follower = new RamseteFollower(Constants.WHEEL_BASE, traj);
         System.out.println("Created new Follower");
+        trackingThread = new Notifier(() -> {
+            isFinished = follower.isFinished();
+            follower.setOdometry(Robot.kDrivetrain.getOdometry());
+            System.out.println("Getting next DriveSignal");
+            System.out.println(follower.getNextDriveSignal().getLeft() + ", " + follower.getNextDriveSignal().getRight());
+            Robot.kDrivetrain.setFPS(follower.getNextDriveSignal().getLeft(), follower.getNextDriveSignal().getRight());
+        });
     }
 
     protected void initialize() {
         Robot.kDrivetrain.setTransmissionHigh();
         start = System.nanoTime();
+        trackingThread.startPeriodic(0.02);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        follower.setOdometry(Robot.kDrivetrain.getOdometry());
-        System.out.println("Getting next DriveSignal");
-        System.out.println(follower.getNextDriveSignal().getLeft() + ", " + follower.getNextDriveSignal().getRight());
-        Robot.kDrivetrain.setFPS(follower.getNextDriveSignal().getLeft(), follower.getNextDriveSignal().getRight());
+        
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return follower.isFinished();
+        return isFinished;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+        trackingThread.stop();
         System.out.println("Finished!");
         System.out.println("Time to completion: " + ((System.nanoTime()-start)/1000000));
     }
